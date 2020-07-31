10050b1ad99074c4594a2c3addec298e05ba4a74
Sergey Atroshchenko <kapxapot@gmail.com>
2020-07-31 21:48:06 +0300

    Array & strpos

--- array.php
+++ array.php
@@ -4,3 +4,11 @@
 //var_dump([$a[0] ?? null, $a[1] ?? null, $a[2] ?? null]);
 
 var_dump(implode('<br/><br/>', []));
+
+$a = [1];
+
+var_dump($a[1] ?? null);
+
+[$first, $second] = $a;
+
+var_dump([$first, $second]); // notice
---
+++ strpos.php
@@ -0,0 +1,4 @@
+<?php
+
+var_dump(strpos('asdasdg', '/start') == 0);
+var_dump(strpos('asdasdg', '/start') === 0);
