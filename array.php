638f0d005b8f16837462dc703fefd614adc350d6
Sergey Atroshchenko <kapxapot@gmail.com>
2020-08-02 00:24:10 +0300

    array_merge test

--- array.php
+++ array.php
@@ -1,14 +1,30 @@
 <?php
 
-//$a = [0, 1];
-//var_dump([$a[0] ?? null, $a[1] ?? null, $a[2] ?? null]);
+echo '1. nulls' . PHP_EOL;
+
+$a = [0, 1];
+var_dump([$a[0] ?? null, $a[1] ?? null, $a[2] ?? null]);
+
+echo '2. implode' . PHP_EOL;
 
 var_dump(implode('<br/><br/>', []));
 
+echo '3. decomposition' . PHP_EOL;
+
 $a = [1];
-
-var_dump($a[1] ?? null);
-
 [$first, $second] = $a;
-
 var_dump([$first, $second]); // notice
+
+echo '4. merge' . PHP_EOL;
+
+var_dump(
+    array_merge(
+        [
+            'one' => 1,
+            'two' => 2,
+        ],
+        [
+            'two' => 3,
+        ]
+    )
+);
